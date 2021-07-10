09a87af5f7429bdce44a01242febdb4fcff96522
karishev <71120362+karishev@users.noreply.github.com>
2021-07-10 07:01:00 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ function scrollHeader(){
 }
 window.addEventListener('scroll', scrollHeader)
 
-// const sections = document.querySelectorAll('section[id]')
+// const sections = document.querySelectorAll('.section')
 
 // function scrollActive(){
 //     const scrollY = window.pageYOffset
@@ -82,42 +82,101 @@ var swiper = new Swiper(".mySwiper", {
 	},
 });
 
+assignments = document.querySelectorAll(".assignment");
+assignmentphotos = document.querySelectorAll(".assignmentphoto");
+assignmentinfos = document.querySelectorAll(".assignmentinfo");
+words = document.querySelectorAll(".words1");
+closebuttons = document.querySelectorAll(".closeButton");
+let opened = [false,false,false,false,false,false,false,false,false];
+
+document.querySelectorAll(".assignment").forEach(assignemnt => 
+	assignemnt.addEventListener("click", () => {
+		let indexx = whichone(assignemnt);
+		if (!opened[indexx]) {
+			assignemnt.style.width = "100%";
+			assignemnt.style.height = "100%";
+			
+			words[indexx].style.display = "none";
+			setTimeout (() => {
+				assignmentphotos[indexx].style.display = "flex";
+				assignmentinfos[indexx].style.display = "flex";
+			}, 500);
+			setTimeout (() => {
+				closebuttons[indexx].style.display = "block";
+			}, 1000);
+			opened[indexx] = true;
+		}
+	})
+)
+
+document.querySelectorAll(".closeButton").forEach(button =>
+	button.addEventListener("click", () => {
+		let index1 = whichonebutton(button);
+		assignments[index1].style.width = "50%";
+		assignments[index1].style.height = "70%";
+		assignmentphotos[index1].style.display = "none";
+		assignmentinfos[index1].style.display = "none";
+		button.style.display = "none";
+		setTimeout(() => {
+			words[index1].style.display = "block";
+			opened[index1] = false;
+		}, 1000)
+	})
+)
+
 let step1 = document.querySelector('.assignment1');
 let step1show = document.querySelectorAll(".step2show");
 let closeButton1 = document.getElementById('closeButton2');
 let opened1 = false;
-step1.addEventListener('click', function() {
-	if (!opened1) {
-		step1.style.width = "80%";
-		step1.style.height = "100%";
-		document.querySelector('.words1').style.display = "none";
-		setTimeout (() => {
-			for (var i = 0; i<2; i++) {
-				step1show[i].style.display = "flex";
-			}
-		}, 200);
-		setTimeout (() => {
-			step1show[2].style.display = "block";
-		}, 1000);
-		// step1.style.background = "#373737";
-		opened1 = true;
+
+function whichone(elem) {
+	for (var i = 0; i < assignments.length; i++) {
+		if (assignments[i] === elem) {
+			return i;
+		}
 	}
-});
+}
+
+function whichonebutton(elem) {
+	for (var i = 0; i < closebuttons.length; i++) {
+		if (closebuttons[i] === elem) {
+			return i;
+		}
+	}
+}
+
+// step1.addEventListener('click', function() {
+// 	if (!opened1) {
+// 		step1.style.width = "80%";
+// 		step1.style.height = "100%";
+// 		document.querySelector('.words1').style.display = "none";
+// 		setTimeout (() => {
+// 			for (var i = 0; i<2; i++) {
+// 				step1show[i].style.display = "flex";
+// 			}
+// 		}, 200);
+// 		setTimeout (() => {
+// 			step1show[2].style.display = "block";
+// 		}, 1000);
+// 		// step1.style.background = "#373737";
+// 		opened1 = true;
+// 	}
+// });
 
 
-closeButton1.addEventListener('click', function() {
-	step1.style.width = "50%";
-	step1.style.height = "70%";
+// closeButton1.addEventListener('click', function() {
+// 	step1.style.width = "50%";
+// 	step1.style.height = "70%";
 	
-	for (var i = 0; i<3; i++) {
-		step1show[i].style.display = "none";
-	}
-	// step1.style.background = "#207794";
-	setTimeout(() => {
-		document.querySelector('.words1').style.display = "block";
-		opened1 = false;
-	}, 1000)
-})
+// 	for (var i = 0; i<3; i++) {
+// 		step1show[i].style.display = "none";
+// 	}
+// 	// step1.style.background = "#207794";
+// 	setTimeout(() => {
+// 		document.querySelector('.words1').style.display = "block";
+// 		opened1 = false;
+// 	}, 1000)
+// })
 
 
 
@@ -136,3 +195,52 @@ closeButton1.addEventListener('click', function() {
 
 // document.querySelector('.assignmentphoto').addEventListener('mousemove',update)
 // document.querySelector('.assignmentphoto').addEventListener('touchmove',update)
+// var canvas = document.querySelector("canvas");
+ 
+// canvas.width = window.innerWidth;
+// canvas.height = window.innerHeight;
+
+// var l = canvas.getContext('2d');
+
+// // x and y are the coordinates of the circle
+// // vx and vy are the respective speeds
+// var x = Math.floor(Math.random() * innerWidth);
+// var y = Math.floor(Math.random() * innerHeight);
+// var vx = Math.floor(Math.random() * 2);
+// var vy = Math.floor(Math.random() * 4);
+// var radius = 20;
+
+// move();
+
+// // This function will do the animation
+// function move() {
+// 	requestAnimationFrame(move);
+
+// 	// It clears the specified pixels within
+// 	// the given rectangle
+// 	l.clearRect(0, 0, innerWidth, innerHeight);
+
+// 	// Creating a circle
+// 	l.beginPath();
+// 	l.strokeStyle = "black";
+// 	l.arc(x, y, radius, 0, Math.PI * 2, false);
+// 	l.stroke();
+
+// 	// Conditions so that the ball bounces
+// 	// from the edges
+// 	if (radius + x > innerWidth)
+// 		vx = 0 - vx;
+
+// 	if (x - radius < 0)
+// 		vx = 0 - vx;
+
+// 	if (y + radius > innerHeight)
+// 		vy = 0 - vy;
+
+// 	if (y - radius < 0)
+// 		vy = 0 - vy;
+
+// 	x = x + vx;
+// 	y = y + vy;
+
+// }
